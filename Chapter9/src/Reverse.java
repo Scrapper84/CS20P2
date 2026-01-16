9380fd53271145e3fe1d271155fbd77a6173661b
1100021220 <1100021220@L815WD220032.edu.cbe.ab.ca>
2026-01-16 10:01:45 -0700

    10:01AM

---
+++ Chapter9/src/Reverse.java
@@ -0,0 +1,24 @@
+
+public class Reverse 
+{
+
+	public static void main(String[] args) 
+	{
+		int numElements =10;
+		int[] numbers = new int[numElements];
+		
+		//store numbers in the array
+		
+		
+		
+		
+		
+		
+		//display countdown from the array
+		
+		
+		
+
+	}
+
+}
---
+++ Chapter9/src/how_many_friends.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class how_many_friends 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int numFriends;
+		String[] friends;
+		
+		System.out.print("How many friends?  ");
+	}
+
+}
